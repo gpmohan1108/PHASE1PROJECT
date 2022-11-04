de16bdd35068c156e348ab94666b02223154ca15
Mohan <gpmohan12345@gmail.com>
2022-11-04 06:58:34 +0530

    upload

--- jenkinsproj/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace jenkinsproj.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
